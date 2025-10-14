d7710495278c913b352cac7583a85cce3fbbd8e5
OhseungKeun <sbe03011@naver.com>
2025-10-14 15:39:05 +0900

    3rd commit

--- 02/one-server/main.tf
+++ 02/one-server/main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "example" {
-  ami = "ami-077b630ef539aa0b5"
+  ami           = "ami-077b630ef539aa0b5"
   instance_type = "t3.micro"
   tags = {
     Name = "terraform-example"
